a926b26614d5d5ef2a8ec966680a33aa882673ec
yoshiaki <afmaar128@gmail.com>
2021-08-07 06:41:45 +0900

    fix second header

--- script.js
+++ script.js
@@ -13,4 +13,32 @@ $(function () {
     }
   });
 
+  // 予約モーダル
+  $('.reserve-btn').click(function(){
+    $('#modal-wrapper').fadeIn()
+  });
+  
+  // 予約モーダルを閉じる
+  $('#close').click(function(){
+    $('#modal-wrapper').fadeOut()
+  });
+
+  // タブメニュー
+
+  // ①タブをクリックしたら発動
+  $('.tab li').click(function() {
+ 
+    // ②クリックされたタブの順番を変数に格納
+    var index = $('.tab li').index(this);
+ 
+    // ③クリック済みタブのデザインを設定したcssのクラスを一旦削除
+    $('.tab li').removeClass('active-tab');
+ 
+    // ④クリックされたタブにクリック済みデザインを適用する
+    $(this).addClass('active-tab');
+ 
+    // ⑤コンテンツを一旦非表示にし、クリックされた順番のコンテンツのみを表示
+    $('.area dl').removeClass('show').eq(index).addClass('show');
+  });
+
 });
